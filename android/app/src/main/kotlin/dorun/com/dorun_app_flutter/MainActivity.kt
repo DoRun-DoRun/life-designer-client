6b06c7ba9c3ac6196e727200c9c39208e0131f38
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:23:11 +0900

    init

---
+++ android/app/src/main/kotlin/dorun/com/dorun_app_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package dorun.com.dorun_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: _App(),
+    ),
+  );
+}
+
+class _App extends ConsumerWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      theme: ThemeData(
+        fontFamily: 'NotoSans'
+      ),
+    );
+  }
+}
