5c798a9cced4a179e077bad1d5d1aee3618cb701
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:37:29 +0900

    feat : path to url

---
+++ lib/common/constant/data.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
+const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
+
+// localhost
+final emulatorIp = '10.0.2.2:3000';
+final simulatorIp = '127.0.0.1:3000';
+
+final ip = Platform.isIOS ? simulatorIp : emulatorIp;
