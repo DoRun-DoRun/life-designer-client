5cd37ec0c2eca8e9d1ee0cfbdf8bf3fc8d57a6b9
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:38:08 +0900

    feat: user model

---
+++ lib/features/user/model/user_model.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../common/utils/data_utils.dart';
+
+part 'user_model.g.dart';
+
+abstract class UserModelBase {}
+
+class UserModelError extends UserModelBase {
+  final String message;
+
+  UserModelError({
+    required this.message,
+  });
+}
+
+class UserModelLoading extends UserModelBase {}
+
+@JsonSerializable()
+class UserModel extends UserModelBase {
+  final String id;
+  final String username;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl,
+  )
+  final String imageUrl;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.imageUrl,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+}
