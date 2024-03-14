da0dfc8e42e82917a7e5cf9325fd5a6131c1ea29
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:53:29 +0900

    feat : user me login process

---
+++ lib/features/user/repository/user_me_repository.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../../common/constant/data.dart';
+import '../../../common/dio/dio.dart';
+import '../model/user_model.dart';
+
+part 'user_me_repository.g.dart';
+
+final userMeRepositoryProvider = Provider<UserMeRepository>(
+      (ref) {
+    final dio = ref.watch(dioProvider);
+
+    return UserMeRepository(dio, baseUrl: 'http://$ip/user/me');
+  },
+);
+
+@RestApi()
+abstract class UserMeRepository {
+  factory UserMeRepository(Dio dio, {String baseUrl}) = _UserMeRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<UserModel> getMe();
+
+}
