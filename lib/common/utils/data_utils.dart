8cd47f2e0342566b2d7f8cdaba2ccee32b9a5707
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:47:00 +0900

    feat : plain to base64 encode

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+
 import '../constant/data.dart';
 
 class DataUtils{
   static String pathToUrl(String value){
     return 'http://$ip$value';
+  }
+
+  static String plainToBase64(String plain){
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+    String encoded = stringToBase64.encode(plain);
+
+    return encoded;
   }
 }
