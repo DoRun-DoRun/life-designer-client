589f700fcdfd48c415c1d32b2da3b6113935324e
jmcunst <xnslqjtmghaf@gmail.com>
2024-03-14 16:53:00 +0900

    feat : http response handling

---
+++ lib/common/dio/dio.dart
@@ -0,0 +1,110 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../features/user/provider/auth_provider.dart';
+import '../constant/data.dart';
+import '../secure_storage/secure_storage.dart';
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio();
+
+  final storage = ref.watch(secureStorageProvider);
+
+  dio.interceptors.add(
+    CustomInterceptor(
+      storage: storage,
+      ref: ref,
+    ),
+  );
+
+  return dio;
+});
+
+class CustomInterceptor extends Interceptor {
+  final FlutterSecureStorage storage;
+  final Ref ref;
+
+  CustomInterceptor({
+    required this.storage,
+    required this.ref,
+  });
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+
+    if (options.headers['accessToken'] == 'true') {
+      options.headers.remove('accessToken');
+
+      final token = await storage.read(key: ACCESS_TOKEN_KEY);
+
+      options.headers.addAll({
+        'authorization': 'Bearer $token',
+      });
+    }
+
+    if (options.headers['refreshToken'] == 'true') {
+      options.headers.remove('refreshToken');
+
+      final token = await storage.read(key: REFRESH_TOKEN_KEY);
+
+      options.headers.addAll({
+        'authorization': 'Bearer $token',
+      });
+    }
+
+    return super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    return super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+
+      try {
+        final resp = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+
+        final accessToken = resp.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        options.headers.addAll({
+          'authorization': 'Bearer $accessToken',
+        });
+
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        final response = await dio.fetch(options);
+
+        return handler.resolve(response);
+      } on DioError catch (e) {
+        ref.read(authProvider.notifier).logout();
+        return handler.reject(e);
+      }
+    }
+
+    return handler.reject(err);
+  }
+}
