080a87a74a3575805a118b468fc17a441ad2da86
JMcunst <xnslqjtmghaf@gmail.com>
2024-03-15 14:59:22 +0900

    feat : resource of colors

---
+++ lib/common/constant/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+/* Primary */
+const PRIMARY_COLOR = Color(0xFF5981F1);
+const PRIMARY_DARK_COLOR = Color(0xFF3861D6);
+const PRIMARY_LIGHT_COLOR = Color(0xFFDAE7FF);
+
+/* Base */
+const GREY_COLOR = Color(0xFFA1A1A1);
+const GREY_DARK_COLOR = Color(0xFF666666);
+
+/* Components */
+const BODY_TEXT_COLOR = Color(0xFF868686);
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
