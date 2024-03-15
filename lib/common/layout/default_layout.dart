73102feed014026030e4b2592cbbacb568621e67
JMcunst <xnslqjtmghaf@gmail.com>
2024-03-15 14:56:47 +0900

    feat : default layout

---
+++ lib/common/layout/default_layout.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
+  final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
+  final Widget? floatingActionButton;
+
+  const DefaultLayout({
+    required this.child,
+    this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
+    this.floatingActionButton,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
+      body: child,
+      bottomNavigationBar: bottomNavigationBar,
+      floatingActionButton: floatingActionButton,
+    );
+  }
+
+  AppBar? renderAppBar(){
+    if(title == null){
+      return null;
+    }else{
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
+  }
+}
