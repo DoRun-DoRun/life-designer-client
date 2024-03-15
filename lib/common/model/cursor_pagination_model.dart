21f6d6fd911a8770e00e74a57cb6b3da2a285254
JMcunst <xnslqjtmghaf@gmail.com>
2024-03-15 15:19:28 +0900

    feat : pagination model

---
+++ lib/common/model/cursor_pagination_model.dart
@@ -0,0 +1,80 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cursor_pagination_model.g.dart';
+
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
+@JsonSerializable(
+  genericArgumentFactories: true,
+)
+class CursorPagination<T> extends CursorPaginationBase {
+  final CursorPaginationMeta meta;
+  final List<T> data;
+
+  CursorPagination({
+    required this.meta,
+    required this.data,
+  });
+
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination<T>(
+      meta: meta ?? this.meta,
+      data: data ?? this.data,
+    );
+  }
+
+  factory CursorPagination.fromJson(
+          Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
+      _$CursorPaginationFromJson(json, fromJsonT);
+}
+
+@JsonSerializable()
+class CursorPaginationMeta {
+  final int count;
+  final bool hasMore;
+
+  CursorPaginationMeta({
+    required this.count,
+    required this.hasMore,
+  });
+
+  CursorPaginationMeta copyWith({
+    int? count,
+    bool? hasMore,
+  }) {
+    return CursorPaginationMeta(
+      count: count ?? this.count,
+      hasMore: hasMore ?? this.hasMore,
+    );
+  }
+
+  factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
+      _$CursorPaginationMetaFromJson(json);
+}
+
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
+}
