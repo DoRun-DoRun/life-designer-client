87428a254a36afa718bb2bbedff0420cee214daf
JMcunst <xnslqjtmghaf@gmail.com>
2024-03-15 15:08:17 +0900

    feat : basic root tab

---
+++ lib/common/view/root_tab.dart
@@ -0,0 +1,68 @@
+import 'package:dorun_app_flutter/common/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../layout/default_layout.dart';
+
+class RootTab extends StatefulWidget {
+  static String get routeName => 'home';
+
+  const RootTab({Key? key}) : super(key: key);
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 4, vsync: this);
+
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: '갓생살기',
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          //TODO: Components
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          controller.animateTo(index);
+        },
+        currentIndex: index,
+        items: [
+          //TODO: Components
+        ],
+      ),
+    );
+  }
+}
