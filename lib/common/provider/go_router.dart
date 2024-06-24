060f3d0527947a701cc10dc78d88670083518917
Shinak <hin6150@gmail.com>
2024-06-24 22:04:37 +0900

    [Refactor] Pull Request 디자인 리뷰
    * chore: flutter_localization 설치
    
    * feat: 공통 컴포넌트 추가 및 수정
    
    * fix: 로그인 로직 비활성화
    
    * refactor: 루틴 생성 UI 수정

--- lib/common/provider/go_router.dart
+++ lib/common/provider/go_router.dart
@@ -10,6 +10,6 @@ final routerProvider = Provider<GoRouter>((ref) {
     routes: provider.routes,
     initialLocation: '/',
     refreshListenable: provider,
-    redirect: provider.redirectLogic,
+    // redirect: provider.redirectLogic,
   );
 });
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dorun_app_flutter/common/provider/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(
@@ -23,6 +24,15 @@ class _App extends ConsumerWidget {
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', ''), // 한국어 지원
+        Locale('en', ''), // 영어 지원
+      ],
     );
   }
 }
